9f439ad99715828e61fc658a8935d2a6ee304ded
Kristian Nilssen <kcn0519@westminstercollege.edu>
2022-09-13 19:22:15 -0700

    updating person

--- mlbStatsAPI/__init__.py
+++ mlbStatsAPI/__init__.py
@@ -5,5 +5,8 @@ Python Wrapper for the MLB Stats API - https://statsapi.mlb.com
 from .game import Game
 from .person import Person
 
+game = Game
+person = Person
+
 from .functions import liveGames_print
 from .functions import liveGames_data
