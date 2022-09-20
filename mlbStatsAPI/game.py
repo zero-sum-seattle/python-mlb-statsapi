a90b26dd290661ce80fb15ea645b948b4386886c
KCNilssen <krinilssen@gmail.com>
2022-09-20 13:32:19 -0700

    Added datetime, status, venue, weather properties

--- mlbStatsAPI/game.py
+++ mlbStatsAPI/game.py
@@ -1,5 +1,101 @@
 import requests
+from dataclasses import dataclass, asdict
 
+@dataclass(frozen=True)
+class gameTime:
+    __slots__ = ['dateTime','originalDate','officialDate','dayNight','time','ampm']
+    dateTime: str
+    originalDate: str
+    officialDate: str
+    dayNight: str
+    time: str
+    ampm: str
+
+    def __str__(self) -> str:
+        return f'{self.officialDate} {self.time} {self.ampm}'
+
+    def __repr__(self) -> str:
+        return f'{self.officialDate} {self.time} {self.ampm}'
+
+    def asdict(self):
+        return asdict(self)
+
+@dataclass(frozen=True)
+class Status:
+    __slots__ = ['abstractGameState','codedGameState','detailedState','statusCode',
+                'startTimeTBD','abstractGameCode']
+    abstractGameState: str
+    codedGameState: str
+    detailedState: str
+    statusCode: str
+    startTimeTBD: bool
+    abstractGameCode: str
+
+    def __str__(self) -> str:
+        return f'{self.abstractGameState}: {self.detailedState}'
+
+    def __repr__(self) -> str:
+        return f'{self.abstractGameState}: {self.detailedState}'
+
+    def asdict(self):
+        return asdict(self)
+
+@dataclass(frozen=True)
+class Venue:
+    __slots__ = ['venueId','name','address1','address2','city','state','stateAbbrev',
+                'country','latitude','longitude','timeZone','capacity','turfType',
+                'roofType','leftLine','leftCenter','center','rightCenter','rightLine']
+    venueId: int
+    name: str
+
+    address1: str
+    address2: str
+    city: str
+    state: str
+    stateAbbrev: str
+    country: str
+    latitude: float
+    longitude: float
+
+    timeZone: str
+
+    capacity: int
+    turfType: str
+    roofType: str
+    leftLine: int
+    leftCenter: int
+    center: int
+    rightCenter: int
+    rightLine: int
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    def __repr__(self) -> str:
+        return str(self.name)
+
+    @property
+    def id(self):
+        return self.venueId
+
+    def asdict(self):
+        return asdict(self)
+
+@dataclass(frozen=True)
+class Weather:
+    __slots__ = ['condition','temp','wind']
+    condition: str
+    temp: str
+    wind: str
+
+    def __str__(self) -> str:
+        return str(self.condition)
+
+    def __repr__(self) -> str:
+        return str(self.condition)
+
+    def asdict(self):
+        return asdict(self)
 
 class Game():
 
@@ -40,92 +136,134 @@ class Game():
         gameData = gm['gameData']
         liveData = gm['liveData']
 
-        # GAME gameData
-        self._linescore = liveData['linescore']
-        self._boxscore = liveData['boxscore']
-        self._flags = gameData['flags']
-
-        self.gameType = gameData['game']['type']
-        self.doubleHeader = gameData['game']['doubleHeader']
-        self.gameNumber = gameData['game']['gameNumber']
-
         datetime = gameData['datetime']
-        self.game_dateTime = datetime['dateTime']
-        self.game_date = datetime['officialDate']
-        self.gameDate = self.game_date
-        self.daynight = datetime['dayNight']
-        self.game_time = datetime['time']
+        self._datetime = gameTime (
+            dateTime = datetime['dateTime'],
+            originalDate = datetime['originalDate'],
+            officialDate = datetime['officialDate'],
+            dayNight = datetime['dayNight'],
+            time = datetime['time'],
+            ampm = datetime['ampm']
+        )
 
         status = gameData['status']
-        self.gameState = status['abstractGameState']
-        self.detailedGameState = status['detailedState']
+        self._status = Status (
+            abstractGameState = status['abstractGameState'],
+            codedGameState = status['codedGameState'],
+            detailedState = status['detailedState'],
+            statusCode = status['statusCode'],
+            startTimeTBD = status['startTimeTBD'],
+            abstractGameCode = status['abstractGameCode']
+        )
 
         venue = gameData['venue']
-        self.venue_id = venue['id']
-        self.venue_name = venue['name']
-        self.venue_city = venue['location']['city']
-        self.venue_state = venue['location']['state']
-        self.field_info = venue['fieldInfo']
+        self._venue = Venue (
+            venueId = venue['id'],
+            name = venue['name'],
+
+            address1 = venue["location"]['address1'],
+            address2 = venue["location"].get('address2',''),
+            city = venue["location"]['city'],
+            state = venue["location"]['state'],
+            stateAbbrev = venue["location"]['stateAbbrev'],
+            country = venue["location"]['country'],
+            latitude = venue["location"]["defaultCoordinates"]['latitude'],
+            longitude = venue["location"]["defaultCoordinates"]['longitude'],
+
+            timeZone = venue["timeZone"]['tz'],
+
+            capacity = venue["fieldInfo"]['capacity'],
+            turfType = venue["fieldInfo"]['turfType'],
+            roofType = venue["fieldInfo"]['roofType'],
+            leftLine = venue["fieldInfo"]['leftLine'],
+            leftCenter = venue["fieldInfo"]['leftCenter'],
+            center = venue["fieldInfo"]['center'],
+            rightCenter = venue["fieldInfo"]['rightCenter'],
+            rightLine = venue["fieldInfo"]['rightLine']
+        )
 
         weather = gameData['weather']
-        self.weather_condition = weather['condition']
-        self.temperature = weather['temp']
-        self.wind = weather['wind']
-
-        gameInfo = gameData['gameInfo']
-        self.attendance = gameData.get('gameInfo', {}).get('attendance', '-')
-        self.gameDuration = gameData.get('gameInfo', {}).get('gameDurationMinutes', '-')
-
-
-        # Game Team data
-        away = gameData['teams']['away']
-        _away_score_data = self._linescore['teams']['away']
-        self._away_info = away
-        self.away_id = away['id']
-        # self.away_fullName =
-        # self.away_clubName =
-        # self.away_abbrv =
-        # self.away_runs =
-        # self.away_hits =
-        # self.away_errs =
-
-        self._away_team_full = away['name']
-        self._away_team = away['clubName']
-        self._away_team_abbrv = away['abbreviation']
-
-        home = gameData['teams']['home']
-        _home_score_data = self._linescore['teams']['home']
-        self._home_info = home
-        self.home_id = home['id']
-        # self.away_fullName =
-        # self.away_clubName =
-        # self.away_abbrv =
-        # self.away_runs =
-        # self.away_hits =
-        # self.away_errs =
-
-
-        self._home_team_full = home['name']
-        self._home_team = home['clubName']
-        self._home_team_abbrv = home['abbreviation']
-
-
-
-
-
-        # Game liveData
-        self._allPlays = liveData['plays']['allPlays']
-        self._currentPlay = liveData['plays']['currentPlay']
-
-
-
-
-
-
-        # def print_game_plays():
+        self._weather = Weather (
+            condition = weather['condition'],
+            temp = weather['temp'],
+            wind = weather['wind']
+        )
 
 
     @property
     def gameId(self) -> int:
         """game Pk ID number"""
         return self.__gameId
+
+    @property
+    def datetime(self):
+        """datetime dataclass
+
+        Keys/Attributes:
+        ------------
+        dateTime:       str
+        originalDate:   str
+        officialDate:   str
+        dayNight:       str
+        time:           str
+        ampm:           str
+        """
+        return self._datetime
+
+    @property
+    def status(self):
+        """status dataclass
+
+        Keys/Attributes:
+        ------------
+        abstractGameState:  str
+        codedGameState:     str
+        detailedState:      str
+        statusCode:         str
+        startTimeTBD:       bool
+        abstractGameCode:   str
+        """
+        return self._status
+
+    @property
+    def venue(self):
+        """venue dataclass
+
+        Keys/Attributes:
+        ------------
+        venueId:        int
+        name:           str
+
+        address1:       str
+        address2:       str
+        city:           str
+        state:          str
+        stateAbbrev:    str
+        country:        str
+        latitude:       float
+        longitude:      float
+
+        timeZone:       str
+
+        capacity:       int
+        turfType:       str
+        roofType:       str
+        leftLine:       int
+        leftCenter:     int
+        center:         int
+        rightCenter:    int
+        rightLine:      int
+        """
+        return self._venue
+
+    @property
+    def weather(self):
+        """weather dataclass
+
+        Keys/Attributes:
+        ------------
+        condition:  str
+        temp:       str
+        wind:       str
+        """
+        return self._weather
