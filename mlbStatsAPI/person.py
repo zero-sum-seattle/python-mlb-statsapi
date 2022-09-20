b71db5e04604eda2f045ee1463924823b68f6a11
KCNilssen <krinilssen@gmail.com>
2022-09-19 17:08:01 -0700

    clean up

--- mlbStatsAPI/person.py
+++ mlbStatsAPI/person.py
@@ -2,6 +2,195 @@ import requests
 from . import objects as objs
 from dataclasses import dataclass, asdict
 
+class MlbDataWrapper:
+    """### Data wrapper
+    Namespace/subscriptable object for retrieving nested data
+    """
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __getitem__(self, __name: str):
+        return getattr(self, __name)
+
+    def __call__(self, _attr):
+        return getattr(self, _attr)
+
+class StatGroup(MlbDataWrapper):
+    def __init__(
+        self,
+        career_regular = None,
+        career_advanced = None,
+        season_regular = None,
+        season_advanced = None,
+        yearByYear_regular = None,
+        yearByYear_advanced = None,
+        sabermetrics = None,
+        expectedStatistics = None,
+    ):
+
+        self.__regular = MlbDataWrapper(
+            career=career_regular, season=season_regular, yearByYear=yearByYear_regular
+        )
+        self.__advanced = MlbDataWrapper(
+            career=career_advanced, season=season_advanced, yearByYear=yearByYear_advanced
+        )
+
+        self.__career = MlbDataWrapper(regular=career_regular, advanced=career_advanced)
+        self.__season = MlbDataWrapper(regular=season_regular, advanced=season_advanced)
+        self.__yearByYear = MlbDataWrapper(regular=yearByYear_regular, advanced=yearByYear_advanced)
+
+        self.__sabermetrics = sabermetrics
+        self.__expectedStatistics = expectedStatistics
+
+    @property
+    def season(self):
+        return self.__season
+
+    @property
+    def career(self):
+        return self.__career
+
+    @property
+    def yearByYear(self):
+        return self.__yearByYear
+
+    @property
+    def regular(self):
+        return self.__regular
+
+    @property
+    def standard(self):
+        return self.__regular
+
+    @property
+    def advanced(self):
+        return self.__advanced
+
+    @property
+    def sabermetrics(self):
+        return self.__sabermetrics
+
+    @property
+    def expectedStatistics(self):
+        return self.__expectedStatistics
+
+
+class PlayerStats(MlbDataWrapper):
+    def __init__(
+        self,
+        season_hitting = None,
+        season_pitching = None,
+        season_fielding = None,
+        seasonAdvanced_hitting = None,
+        seasonAdvanced_pitching = None,
+        seasonAdvanced_fielding = None,
+        career_hitting = None,
+        career_pitching = None,
+        career_fielding = None,
+        careerAdvanced_hitting = None,
+        careerAdvanced_pitching = None,
+        careerAdvanced_fielding = None,
+        yearByYear_hitting = None,
+        yearByYear_pitching = None,
+        yearByYear_fielding = None,
+        yearByYearAdvanced_hitting = None,
+        yearByYearAdvanced_pitching = None,
+        yearByYearAdvanced_fielding = None,
+        sabermetrics_hitting = None,
+        sabermetrics_pitching = None,
+        sabermetrics_fielding = None,
+        expectedStatistics_hitting = None,
+        expectedStatistics_pitching = None,
+        expectedStatistics_fielding = None,
+        sprayChart = None,
+        hotColdZones = None
+    ):
+        super().__init__(
+            **{
+                "season_hitting" : season_hitting,
+                "season_pitching" : season_pitching,
+                "season_fielding" : season_fielding,
+                "seasonAdvanced_hitting" : seasonAdvanced_hitting,
+                "seasonAdvanced_pitching" : seasonAdvanced_pitching,
+                "seasonAdvanced_fielding" : seasonAdvanced_fielding,
+                "career_hitting" : career_hitting,
+                "career_pitching" : career_pitching,
+                "career_fielding" : career_fielding,
+                "careerAdvanced_hitting" : careerAdvanced_hitting,
+                "careerAdvanced_pitching" : careerAdvanced_pitching,
+                "careerAdvanced_fielding" : careerAdvanced_fielding,
+                "yearByYear_hitting" : yearByYear_hitting,
+                "yearByYear_pitching" : yearByYear_pitching,
+                "yearByYear_fielding" : yearByYear_fielding,
+                "yearByYearAdvanced_hitting" : yearByYearAdvanced_hitting,
+                "yearByYearAdvanced_pitching" : yearByYearAdvanced_pitching,
+                "yearByYearAdvanced_fielding" : yearByYearAdvanced_fielding,
+                "sabermetrics_hitting" : sabermetrics_hitting,
+                "sabermetrics_pitching" : sabermetrics_pitching,
+                "sabermetrics_fielding" : sabermetrics_fielding,
+                "expectedStatistics_hitting" : expectedStatistics_hitting,
+                "expectedStatistics_pitching" : expectedStatistics_pitching,
+                "expectedStatistics_fielding" : expectedStatistics_fielding,
+                "sprayChart" : sprayChart,
+                "hotColdZones" : hotColdZones
+            }
+        )
+
+        self.__hitting = StatGroup(
+            season_regular=season_hitting,
+            season_advanced=seasonAdvanced_hitting,
+            career_regular=career_hitting,
+            career_advanced=careerAdvanced_hitting,
+            yearByYear_regular=yearByYear_hitting,
+            yearByYear_advanced=yearByYearAdvanced_hitting,
+            sabermetrics=sabermetrics_hitting,
+            expectedStatistics=expectedStatistics_hitting,
+        )
+        self.__pitching = StatGroup(
+            season_regular=season_pitching,
+            season_advanced=seasonAdvanced_pitching,
+            career_regular=career_pitching,
+            career_advanced=careerAdvanced_pitching,
+            yearByYear_regular=yearByYear_pitching,
+            yearByYear_advanced=yearByYearAdvanced_pitching,
+            sabermetrics=sabermetrics_pitching,
+            expectedStatistics=expectedStatistics_pitching,
+        )
+        self.__fielding = StatGroup(
+            season_regular=season_fielding,
+            career_regular=career_fielding,
+            yearByYear_regular=yearByYear_fielding,
+        )
+        self.__sprayChart = sprayChart
+        self.__hotColdZones = hotColdZones
+
+    @property
+    def hitting(self):
+        """Player's hitting stats (regular or advanced)"""
+        return self.__hitting
+
+    @property
+    def pitching(self):
+        """Player's pitching stats (regular or advanced)"""
+        return self.__pitching
+
+    @property
+    def fielding(self):
+        """Player's fielding stats"""
+        return self.__fielding
+
+    @property
+    def sprayChart(self):
+        """Player's sprayChart stats"""
+        return self.__sprayChart
+
+    @property
+    def hotColdZones(self):
+        """Player's hotColdZones stats"""
+        return self.__hotColdZones
+
+
 @dataclass(frozen=True)
 class PersonName:
     __slots__ = ['personId','full','first','middle','last','boxscore','use','nick','pronunciation']
@@ -119,7 +308,7 @@ class Person:
         )
 
         self._active = prsn['active']
-        self._draftYear = prsn['draftYear']
+        self._draftYear = prsn.get('draftYear','')
         self._mlbDebutDate = prsn['mlbDebutDate']
 
 
