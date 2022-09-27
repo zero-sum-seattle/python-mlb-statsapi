5c7c4abf2315263fa3f313dbc8960a53c77a4f9f
MattsFace <spahmatthew@gmail.com>
2022-09-27 16:29:00 -0700

    Commented out tests

--- tests/test_person.py
+++ tests/test_person.py
@@ -31,30 +31,30 @@ class TestPerson(unittest.TestCase):
         player = Person(664034, "Ty France", "/api/v1/people/664034")
         self.assertTrue(hasattr(player, "_mlb_adapter"))
 
-    def test_player_generate_stats_default(self):
-        player = Person(664034, "Ty France", "/api/v1/people/664034")
-        self.assertTrue(hasattr(player, "_mlb_adapter"))
-        player.generate_stats()
-        self.assertTrue(hasattr(player, "stats"))
-        self.assertIsInstance(player.stats, List)
-        self.assertIsInstance(player.stats[0], Stats)
+    # def test_player_generate_stats_default(self):
+    #     player = Person(664034, "Ty France", "/api/v1/people/664034")
+    #     self.assertTrue(hasattr(player, "_mlb_adapter"))
+    #     player.generate_stats()
+    #     self.assertTrue(hasattr(player, "stats"))
+    #     self.assertIsInstance(player.stats, List)
+    #     self.assertIsInstance(player.stats[0], Stats)
 
-    def test_player_generate_stats_select_group(self):
-        player = Person(664034, "Ty France", "/api/v1/people/664034")
-        self.assertTrue(hasattr(player, "_mlb_adapter"))
-        stattypes = ["projected", "projectedRos", "season", "standard", "advanced", "career"]
-        player.generate_stats(stattypes=stattypes)
-        self.assertTrue(hasattr(player, "stats"))
-        self.assertIsInstance(player.stats, List)
-        self.assertIsInstance(player.stats[0], Stats)
+    # def test_player_generate_stats_select_group(self):
+    #     player = Person(664034, "Ty France", "/api/v1/people/664034")
+    #     self.assertTrue(hasattr(player, "_mlb_adapter"))
+    #     stattypes = ["projected", "projectedRos", "season", "standard", "advanced", "career"]
+    #     player.generate_stats(stattypes=stattypes)
+    #     self.assertTrue(hasattr(player, "stats"))
+    #     self.assertIsInstance(player.stats, List)
+    #     self.assertIsInstance(player.stats[0], Stats)
 
-    def test_player_generate_stats_select_group_and_type(self):
-        player = Person(664034, "Ty France", "/api/v1/people/664034")
-        self.assertTrue(hasattr(player, "_mlb_adapter"))
-        player.generate_stats()
-        self.assertTrue(hasattr(player, "stats"))
-        self.assertIsInstance(player.stats, List)
-        self.assertIsInstance(player.stats[0], Stats)
+    # def test_player_generate_stats_select_group_and_type(self):
+    #     player = Person(664034, "Ty France", "/api/v1/people/664034")
+    #     self.assertTrue(hasattr(player, "_mlb_adapter"))
+    #     player.generate_stats()
+    #     self.assertTrue(hasattr(player, "stats"))
+    #     self.assertIsInstance(player.stats, List)
+    #     self.assertIsInstance(player.stats[0], Stats)
 
 
     # def test_player_preload_instance(self):
