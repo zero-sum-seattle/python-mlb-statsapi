7b5a906bf1f23b9414d6eaf7f8466331c3d2e6da
KCNilssen <krinilssen@gmail.com>
2022-10-05 15:04:00 -0700

    Add General json outlier util

--- utils/find_json_outliers.py
+++ utils/find_json_outliers.py
@@ -1,18 +1,17 @@
 import requests
-import pprint
 from typing import List, Dict
 import json
 import os
-
+import sys
 
 def merge_dictionaries(dict1, dict2):
     merged_dictionary = {}
 
     for key in dict1:
         # print (key)
-        merged_dictionary[key] = {}
+        merged_dictionary[key] = {"occurs#":0, "dictitems":{}}
         if key in dict2:
-            new_count = dict1[key]['#occurs#'] + dict2[key]['#occurs#']
+            new_count = dict1[key]['occurs#'] + dict2[key]['occurs#']
 
             if isinstance(dict1[key]['dictitems'], Dict) and isinstance(dict2[key]['dictitems'], Dict):
                 merged_dictionary[key]['dictitems'] = merge_dictionaries(dict1[key]['dictitems'], dict2[key]['dictitems'])
@@ -23,10 +22,10 @@ def merge_dictionaries(dict1, dict2):
             else:
                 merged_dictionary[key]['dictitems'] = {}
         else:
-            new_count = dict1[key]['#occurs#']
+            new_count = dict1[key]['occurs#']
             merged_dictionary[key]['dictitems'] = dict1[key]['dictitems']
 
-        merged_dictionary[key]['#occurs#'] = new_count
+        merged_dictionary[key]['occurs#'] = new_count
 
     for key in dict2:
         if key not in merged_dictionary:
@@ -41,12 +40,14 @@ def get_reps(r):
 
     if isinstance(r, Dict):
         for key, value in r.items():
-            temp_dict[key] = {'#occurs#':1, 'dictitems':get_reps(value)}
+            temp_dict[key] = {'occurs#':1, 'dictitems':get_reps(value)}
 
         return temp_dict
 
     elif isinstance(r, List):
+
         list_temp_dict = {}
+
         for item in r:
             if isinstance(item, Dict):
                 list_temp_dict = merge_dictionaries(get_reps(item), list_temp_dict)
@@ -59,35 +60,28 @@ def get_reps(r):
 
 def main():
 
-    list_of_game_pks = [662242]
+    list_of_endpoints = ["/api/v1/people/664034",
+                         "/api/v1/people/668881",
+                         "/api/v1/people/668804"
+                         ]
 
-    print ("Adding gamepk: 662242 With progress: Final")
-
-    r = requests.get("https://statsapi.mlb.com/api/v1/schedule?sportId=1").json()
-
-    live_games = []
-    if r.get("totalItems") != 0:
-        for date in r.get("dates"):
-            for game in date.get("games"):
-                print ("Adding gamepk: ", game["gamePk"], "With progress:", game["status"]["detailedState"])
-                list_of_game_pks.append(game["gamePk"])
-
-    print ()
-
-    fp = "games_json_counting.json"
+    fp = "json_counting.json"
 
     result_dict = {}
 
-    for game in list_of_game_pks:
-        r = requests.get(f"https://statsapi.mlb.com/api/v1.1/game/{game}/feed/live").json()
-        print ("merging in game:", game)
-        result_dict = merge_dictionaries(get_reps(r), result_dict)
+    for end_point in list_of_endpoints:
+        if end_point[:7]=='/api/v1':
+            url = "https://statsapi.mlb.com" + end_point
+            print ("Grabbing and joing ", url)
+            endpointData = requests.get(url).json()
+            result_dict = merge_dictionaries(get_reps(endpointData), result_dict)
+        else:
+            print ("bad endpoint Nothing done with:", end_point)
 
     data = json.dumps(json.loads(json.dumps(result_dict)), indent=4)
 
     with open(fp, "w+", newline="", encoding="UTF-8") as f:
             f.write(data)
-
 
 if __name__ == '__main__':
     main()
