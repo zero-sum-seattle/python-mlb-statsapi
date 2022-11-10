92751fac30644cd038c0a05d409b9dd1e570edb2
MattsFace <spahmatthew@gmail.com>
2022-11-10 01:25:39 -0800

    Updated mlb class with syntax improvements related to readability.

--- mlbstatsapi/mlb.py
+++ mlbstatsapi/mlb.py
@@ -2,7 +2,8 @@ from typing import Union, List, Dict
 import importlib
 import inspect
 
-def _transform_mlbdata(mlb_dict, mlb_keys: Union[List[Union[dict, str]], str]) -> dict:
+
+def _transform_mlb_data(mlb_dict, mlb_keys: Union[List[Union[dict, str]], str]) -> dict:
     """
     merge requested nested dicts inside mlb_dict into mlb_dict base. 
 
@@ -23,18 +24,19 @@ def _transform_mlbdata(mlb_dict, mlb_keys: Union[List[Union[dict, str]], str]) -
 
             if isinstance(key, Dict):      
                 for nested_key in key:                       
-                    mlbmergeitem = mlb_dict.pop(nested_key)
-                    mlb_dict.update(_transform_mlbdata(mlbmergeitem, key[nested_key]))            
+                    mlb_merge_item = mlb_dict.pop(nested_key)
+                    mlb_dict.update(_transform_mlb_data(mlb_merge_item, key[nested_key]))
             else:
-                mlbmergeitem = mlb_dict.pop(key)
-                mlb_dict.update(**mlbmergeitem)
+                mlb_merge_item = mlb_dict.pop(key)
+                mlb_dict.update(**mlb_merge_item)
     else:
-        mlbmergeitem = mlb_dict.pop(mlb_keys)
-        mlb_dict.update(**mlbmergeitem)
+        mlb_merge_item = mlb_dict.pop(mlb_keys)
+        mlb_dict.update(**mlb_merge_item)
 
     return mlb_dict
 
-def _return_splits(split_data : dict, stat_type : str, stat_group : str) -> List['Splits']:
+
+def _return_splits(split_data: dict, stat_type: str, stat_group: str) -> List['Splits']:
     """
     The split objects are built using the group name and split data. The stat group name is used to source the correct
     stat group classes. 
@@ -62,16 +64,16 @@ def _return_splits(split_data : dict, stat_type : str, stat_group : str) -> List
 
     # if splits is empty let's jump out
     if not ('splits' in split_data and split_data['splits']):
-         return splits 
+        return splits
 
     for name, obj in inspect.getmembers(stat_module):
-            # type_ attribute holds the stat_type of the class
+        # type_ attribute holds the stat_type of the class
         if inspect.isclass(obj) and (hasattr(obj, '_stat') and stat_type in obj._stat):
             for split in split_data['splits']:
 
                 # if stat_type is in stat_log_type
                 # do required dictionary transformation
-                if (stat_type in stat_log_type):
+                if stat_type in stat_log_type:
                     split = _transform_mlbdata(split, [{'stat':'play'}])
 
                 # if stat is in split merge
