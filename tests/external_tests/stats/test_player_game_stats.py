40a8ac79cf9395f0428b2d3981da5ec3e962653f
MattsFace <spahmatthew@gmail.com>
2022-11-27 13:39:27 -0800

    Added mock JSON and External Tests for game stats

---
+++ tests/external_tests/stats/test_player_game_stats.py
@@ -0,0 +1,30 @@
+﻿import unittest
+import time
+
+from mlbstatsapi.mlb_api import Mlb
+
+
+class TestHittingStats(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.mlb = Mlb()
+        cls.al_team = 133
+        cls.shoei_ohtani = 660271
+        cls.game_id = 531368
+                    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    
+    def test_get_players_stats_for_game(self, m):
+        """return player stat objects"""
+
+        game_stats = self.mlb.get_players_stats_for_game(person_id=self.shoei_ohtani,
+                                                         game_id=self.game_id)
+
+        # game stats should not be None
+        self.assertIsNotNone(game_stats)
+
+        # game_stats should be a dict
+        self.assertIsInstance(game_stats, dict)
