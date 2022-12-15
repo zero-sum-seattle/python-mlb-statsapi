e2d8c6a1559521f85b6edf344fbe52149fbedcf5
KCNilssen <krinilssen@gmail.com>
2022-12-15 14:39:27 -0800

    seed docstring

--- mlbstatsapi/models/homerunderby/attributes.py
+++ mlbstatsapi/models/homerunderby/attributes.py
@@ -242,13 +242,13 @@ class Seed:
     winner : bool
         A boolean indicating whether the player for this seed is the winner 
         of the game.
-    player : Union[Person, dict]
+    player : Person
         An object containing the data for the player associated with this 
         seed. This can either be a Person object or a dictionary.
-    topderbyhitdata : Union[Hitdata, dict]
+    topderbyhitdata : Hitdata
         An object containing the data for the top hit in the seed. This can 
         either be a Hitdata object or a dictionary.
-    hits : Union[Hits, dict]
+    hits : Hits
         An object containing the data for the hits in the seed. This can 
         either be a Hits object or a dictionary.
     seed : int
