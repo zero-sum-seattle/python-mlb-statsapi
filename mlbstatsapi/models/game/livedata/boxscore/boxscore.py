87ad299bd26a7a390cf0da14cd3e87498d69d7be
Matthew Spah <mspah@dualpath.net>
2024-02-01 01:46:36 -0800

    I'll visit this and rebase

--- mlbstatsapi/models/game/livedata/boxscore/boxscore.py
+++ mlbstatsapi/models/game/livedata/boxscore/boxscore.py
@@ -1,7 +1,33 @@
-from typing import Union, List
+from typing import Union, List, Any, Optional
 from dataclasses import dataclass, field
 
-from .attributes import BoxScoreTeams, BoxScoreOffical, BoxScoreVL
+from .attributes import BoxScoreTeams, BoxScoreOffical, BoxScoreVL, PlayersDictPerson
+
+
+
+@dataclass
+class TopPerformer:
+    """
+    A class to represent this games topperformer
+
+    Attributes
+    ----------
+    player : Player
+        Player 
+    type : str
+        The officials for this game
+    gamescore : int
+        gamescore
+    hittinggamescore : int
+        hitting game score
+    """
+    player: Union[PlayersDictPerson, dict]
+    type: str
+    gamescore: int
+    hittinggamescore: int
+
+    def __post_init__(self):
+        self.player = PlayersDictPerson(**self.player)
 
 @dataclass
 class BoxScore:
@@ -19,12 +45,17 @@ class BoxScore:
     pitchingnotes : List[str]
         Pitching notes for this game
     """
+
     teams: Union[BoxScoreTeams, dict]
     officials: Union[List[BoxScoreOffical], List[dict]]
     info: Union[List[BoxScoreVL], List[dict]]
     pitchingnotes: List[str]
+    topperformers: Optional[List[Union[TopPerformer, dict]]] = field(default_factory=dict)
 
     def __post_init__(self):
         self.teams = BoxScoreTeams(**self.teams)
         self.officials = [BoxScoreOffical(**official) for official in self.officials]
         self.info = [BoxScoreVL(**infos) for infos in self.info]
+        self.topperformers = [TopPerformer(**topperformer) for topperformer in self.topperformers]
+
+
